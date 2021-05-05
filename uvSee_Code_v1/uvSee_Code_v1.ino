5c7f7f818ea388c7ec8b6a1049bca2aa98014f1d
Prasannaa Kumar <70738433+prasannaad@users.noreply.github.com>
2021-05-05 17:29:03 +0530

    Add files via upload

---
+++ uvSee_Code_v1/uvSee_Code_v1.ino
@@ -0,0 +1,78 @@
+#include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+int UVOUT = 15; //Output from the sensor
+int REF_3V3 = 4; //3.3V power on the Arduino board
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  SerialBT.begin("uvSee"); //Bluetooth device name
+
+  pinMode(UVOUT, INPUT);
+  pinMode(REF_3V3, INPUT);
+
+  Serial.println("ML8511 example");
+}
+
+void loop()
+{
+  
+  if (SerialBT.available())
+  {
+  String in = SerialBT.readString();
+  if (in == "read")
+  {
+  int uvLevel = averageAnalogRead(UVOUT);
+  int refLevel = averageAnalogRead(REF_3V3);
+
+  //Use the 3.3V power pin as a reference to get a very accurate output value from sensor
+  float outputVoltage = 3.3 / refLevel * uvLevel;
+
+  float uvIntensity = mapfloat(outputVoltage, 0.99, 2.8, 0.0, 15.0); //Convert the voltage to a UV intensity level
+
+  Serial.print("output: ");
+  Serial.print(refLevel);
+
+  Serial.print("ML8511 output: ");
+  Serial.print(uvLevel);
+
+  Serial.print(" / ML8511 voltage: ");
+  Serial.print(outputVoltage);
+
+  Serial.print(" / UV Intensity (mW/cm^2): ");
+  Serial.print(uvIntensity);
+
+  Serial.println();
+
+  delay(500);
+  
+  }
+  }
+}
+
+//Takes an average of readings on a given pin
+//Returns the average
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 8;
+  unsigned int runningValue = 0; 
+
+  for(int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
+
+  return(runningValue);  
+}
+
+//The Arduino Map function but for floats
+//From: http://forum.arduino.cc/index.php?topic=3922.0
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
